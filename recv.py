72aad4968311e0d6406d2097aa51e7081f51e586
= <sepand.haghighi@yahoo.com>
2016-02-01 18:55:53 +0330

    feat:log added

--- recv.py
+++ recv.py
@@ -2,11 +2,17 @@ import socket
 import sys
 UDP_IP = ''
 UDP_PORT =4660
-
+file=open("recieve_log.txt","a")
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
 sock.bind((UDP_IP,UDP_PORT))
+
 while (True):
     data=sock.recvfrom(1024)
     print(str(data[0])[2:-1])
+    file.write(str(data[0])[2:-1]+"\n")
+    if str(data[0])[2:-1].upper()=="EXIT":
+        file.close()
+        sock.close()
+        break
 sock.close()
+file.close()
--- send.py
+++ send.py
@@ -4,6 +4,7 @@ import subprocess
 current_direct=os.getcwd()
 socket.socket.accept
 my_ip=socket.gethostbyname(socket.gethostname())
+file=open("chat_log.txt","a")
 print("Your Device IP : " + my_ip)
 ip=input("Please Enter Target Ip: ")
 port=int(input("Please Enter Port : "))
@@ -12,8 +13,10 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #print(value)
 while(True):
     message=input("You : ")
+    file.write("You : "+message+"\n")
     if message.upper()=="EXIT":
         sock.sendto("Terminate".encode("utf-8"),(ip,port))
+        file.close()
         break
     message=my_ip+":"+message
     sock.sendto(message.encode("utf-8"),(ip,port))
